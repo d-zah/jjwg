e2cea5a3663f16a6bccde035b946de370e29be33
Joshua Murph <jmurph4@uncc.edu>
2022-11-17 14:06:40 -0500

    changed files to contain tasks instead of notes, all users can see all tasks now

--- tests.py
+++ tests.py
@@ -7,28 +7,28 @@ class FlaskTest(unittest.TestCase):
         response = requests.get("http://127.0.0.1:5000/")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-        self.assertEqual('<h2>Use this site to maintain and organize your notes.</h2>' in response.text, True)
+        self.assertEqual('<h2>Use this site to maintain and organize your tasks.</h2>' in response.text, True)
 
-    def test_notes(self):
-        response = requests.get("http://127.0.0.1:5000/notes")
+    def test_tasks(self):
+        response = requests.get("http://127.0.0.1:5000/tasks")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
         self.assertEqual('Title' and 'Date' in response.text, True)
 
-    def test_note(self):
-        response = requests.get("http://127.0.0.1:5000/notes/1")
+    def test_task(self):
+        response = requests.get("http://127.0.0.1:5000/tasks/1")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-        self.assertEqual('First Note' in response.text, True)
+        self.assertEqual('First task' in response.text, True)
 
     def test_new(self):
-        response = requests.get("http://127.0.0.1:5000/notes/new")
+        response = requests.get("http://127.0.0.1:5000/tasks/new")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
         self.assertEqual('<form action="new" method="post">' in response.text, True)
 
     def test_delete(self):
-        response = requests.get('http://127.0.0.1:5000/notes/delete')
+        response = requests.get('http://127.0.0.1:5000/tasks/delete')
         statuscode = response.status_code
         self.assertEqual(statuscode, 500)
 
